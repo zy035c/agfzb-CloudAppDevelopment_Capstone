7ac055ec4d5c54a5d057c1279c8910d6265d1aa1
Ziyi Chen <zy35c@gmail.com>
2022-09-10 21:54:23 +0000

    func

--- functions/sample/nodejs/index-async-await.js
+++ functions/sample/nodejs/index-async-await.js
@@ -6,16 +6,17 @@ const { CloudantV1 } = require('@ibm-cloud/cloudant');
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
 async function main(params) {
-      const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
-      const cloudant = CloudantV1.newInstance({
-          authenticator: authenticator
-      });
-      cloudant.setServiceUrl(params.COUCH_URL);
-      try {
-        let dbList = await cloudant.getAllDbs();
-        return { "dbs": dbList.result };
-      } catch (error) {
-          return { error: error.description };
-      }
+    const cloudant = Cloudant({
+        url: params.COUCH_URL,
+        plugins: {
+            iamauth: { iamApiKey: params.IAM_API_KEY }
+        }
+    });
+    try {
+        let dbList = await cloudant.db.list();
+        return { "dbs": dbList }
+    } catch (error) {
+        return { error: error.description };
+    }
 }
 
--- functions/sample/nodejs/index-promise.js
+++ functions/sample/nodejs/index-promise.js
@@ -6,28 +6,30 @@ const { CloudantV1 } = require('@ibm-cloud/cloudant');
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
 function main(params) {
+    // <pre><code>
+        const cloudant = Cloudant({
+            url: params.COUCH_URL,
+            plugins: {
+                iamauth: { iamApiKey: params.IAM_API_KEY }
+            }
+        });
 
-    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
-    const cloudant = CloudantV1.newInstance({
-      authenticator: authenticator
-    });
-    cloudant.setServiceUrl(params.COUCH_URL);
-
-    let dbListPromise = getDbs(cloudant);
-    return dbListPromise;
+        let dbListPromise = getDbs(cloudant)
+        return dbListPromise
+   // </code></pre>
 }
 
 function getDbs(cloudant) {
-     return new Promise((resolve, reject) => {
-         cloudant.getAllDbs()
-             .then(body => {
-                 resolve({ dbs: body.result });
-             })
-             .catch(err => {
-                  console.log(err);
-                 reject({ err: err });
-             });
-     });
+    return new Promise((resolve, reject) => {
+        cloudant.db.list()
+            .then(body => {
+                resolve({ dbs: body });
+            })
+            .catch(err => {
+                console.log(err);
+                reject({ err: err });
+            });
+    });
  }
  
  
