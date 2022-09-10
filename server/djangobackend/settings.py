30ac6fe2ce528e77b35aa4842c153fc0481d55d2
zy035c <91088913+zy035c@users.noreply.github.com>
2022-09-10 19:25:19 -0400

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "zyccapstone.us-south.mybluemix.net"]
+ALLOWED_HOSTS = ["localhost", "zyccapstone.eu-gb.mybluemix.net"]
 
 
 # Application definition
