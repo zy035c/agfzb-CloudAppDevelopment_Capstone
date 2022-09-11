a1c99fb797ac05ae54d84396625cb573769a9ccc
zy035c <91088913+zy035c@users.noreply.github.com>
2022-09-11 17:29:37 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
+    path(route='', view=views.get_dealerships, name='index')
+    
     # path for about view
     path(route='about/', view=views.about, name='about'),
 
