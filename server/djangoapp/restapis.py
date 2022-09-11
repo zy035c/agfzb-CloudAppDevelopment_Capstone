40234d94c6e23585faf0a9d8327942bcc022097d
zy035c <91088913+zy035c@users.noreply.github.com>
2022-09-11 17:13:29 -0400

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,23 +1,44 @@
 import requests
 import json
 # import related models here
+from .models import CarDealer
 from requests.auth import HTTPBasicAuth
-
 
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
-
+def get_request(url, **kwargs):
+    print(kwargs)
+    print("GET from {}".format(url))
+    try:
+        response = requests.get(
+            url, header={'Content-Type': 'application/json'},
+            params=kwargs
+        )
+    except:
+        print("Network exception occued")
+    status_code = response.status_code
+    print("With status {}".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
 
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
-# def get_dealers_from_cf(url, **kwargs):
-# - Call get_request() with specified arguments
-# - Parse JSON results into a CarDealer object list
-
+def get_dealers_from_cf(url, **kwargs):
+    # - Call get_request() with specified arguments
+    # - Parse JSON results into a CarDealer object list
+    results = []
+    json_result = get_request(url)
+    if json_result:
+        dealer = json_result["entries"]
+        for dealer in dealers:
+            dealer_doc = dealer
+            dealer_obj = CarDealer(
+                address = 
+            )
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
 # def get_dealer_by_id_from_cf(url, dealerId):
