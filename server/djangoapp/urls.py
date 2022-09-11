820e0087ece8f2ce1d670b5c95b134235d1f1e89
zy035c <91088913+zy035c@users.noreply.github.com>
2022-09-11 17:54:05 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-    path(route='', view=views.get_dealerships, name='index')
     
     # path for about view
     path(route='about/', view=views.about, name='about'),
@@ -24,10 +23,12 @@ urlpatterns = [
 
     # path for logout
     path(route='logout/', view=views.logout_request, name='logout'),
-
+    
+    # dealership
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
+    path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
     path(route='add_review/', view=views.add_review, name='add_review'),
