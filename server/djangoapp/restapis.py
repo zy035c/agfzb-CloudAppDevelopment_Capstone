5b248ac30ad189f9ed59faf8f82d4e0534decb15
zy035c <91088913+zy035c@users.noreply.github.com>
2022-09-11 20:14:47 -0400

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -39,7 +39,7 @@ def get_request(url, **kwargs):
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
-def post_request(url, json_payload, **kwargs):
+def post_request(url, payload, **kwargs):
     print(kwargs)
     print("POST to {}".formate(url))
     try:
